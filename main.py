3da0164d8d2394038f4d8ccd9c56f83669944d39
Christian Pojoni <dev@pojoni.at>
2021-05-20 04:21:43 +0200

    table

--- main.py
+++ main.py
@@ -1,29 +1,69 @@
 from flask import Flask
 from flask import request
-import json
+from flask import make_response
 from binance.client import Client
 import pymongo
 
 import settings
+
 client = Client(settings.api_key,
                 settings.api_secret)
 app = Flask(__name__)
-mongo = pymongo.MongoClient(settings.db_host, username=settings.db_user, password=settings.db_pass)
+mongo = pymongo.MongoClient(settings.db_host, username=settings.db_user, password=settings.db_pass,
+                            port=settings.db_port)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>flask-webhooks main page</p>"
+def index():
+    mydb = mongo["indicators"]
+    mycol = mydb["aurox"]
 
+    table_start = "<table border=1><thead>"
+    table_end = "</tbody></table>"
+    button = "<p><form action='aurox'><input type='submit' value='Download' /></form></p>"
+    table_data = []
+    first = True
 
-@app.route("/aurox", methods=['POST'])
+    for x in mycol.find({}, {'_id': False}):
+        keys = ['timestampIso', 'pair', 'signal', 'timeUnit', 'confirmed', 'bidPrice', 'askPrice']
+        filtered = {key: x[key] for key in keys}
+        keys, values = zip(*filtered.items())
+
+        if first:
+            table_data.append('<tr><th>' + '</th><th>'.join(keys) + '</th></tr></thead><tbody>')
+        table_data.append('<tr><td>' + '</td><td>'.join(values) + '</td></tr>')
+        first = False
+
+    return table_start + ''.join(table_data) + table_end + button
+
+@app.route("/aurox", methods=['POST', 'GET'])
 def aurox_webhook():
-    aurox_indicator = request.get_json()
-    if aurox_indicator['exchange'] == 'binance':
-        binance_ticker = client.get_ticker(symbol=aurox_indicator['pair'])
-        merged = {**aurox_indicator, **binance_ticker}
+    if request.method == 'POST':
 
+        aurox_indicator = request.get_json()
+        if aurox_indicator['exchange'] == 'binance':
+            binance_ticker = client.get_ticker(symbol=aurox_indicator['pair'])
+            merged = {**aurox_indicator, **binance_ticker}
+
+            mydb = mongo["indicators"]
+            mycol = mydb["aurox"]
+            x = mycol.insert_one(merged)
+        return "success"
+
+    else:
         mydb = mongo["indicators"]
         mycol = mydb["aurox"]
-        x = mycol.insert_one(merged)
+        first = True
+        csv_data = []
+        for x in mycol.find({}, {'_id': False}):
+            keys, values = zip(*x.items())
+            if first:
+                csv_data.append(';'.join(keys))
 
-    return "success"
+            csv_data.append(';'.join(str(v) for v in values))
+            first = False
+        output = make_response("\n".join(csv_data))
+        output.headers["Content-Disposition"] = "attachment; filename=aurox.csv"
+        output.headers["Content-type"] = "text/csv"
+        return output
+
--- settings.sample.py
+++ settings.sample.py
@@ -1,5 +1,6 @@
 api_key = ""
 api_secret = ""
 db_host = "mongo"
+db_port = 27017
 db_user = "root"
 db_pass = "secret"
